52585af651208ed45e5e2e915a3c818169d81665
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-07-18 11:55:55 -0500

    Add Spreadsheet reporting capability

--- gsheets.py
+++ gsheets.py
@@ -1,1 +1,9 @@
 # Librería para conectar con Google Spreadsheets
+import requests
+import time
+
+def report(code,success):
+    date = time.strftime("%c")
+    url = "https://script.google.com/macros/s/AKfycbwI1RbJdElFFRl8FbYlDd0M7DuztFdCUAB8WSmjyZT-mS1zdvMI/exec?date="+str(date)+"&code="+str(code)+"&success="+str(success)
+    response = requests.get(url)
+    return response
